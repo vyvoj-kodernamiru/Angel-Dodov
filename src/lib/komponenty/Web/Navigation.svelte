05da41e73ea3711a769dff3d2719c57a8b399043
Daniel <kodernamiru@gmail.com>
2023-12-04 12:11:19 +0100

    Navigace

---
+++ src/lib/komponenty/Web/Navigation.svelte
@@ -0,0 +1,54 @@
+<div class="navbar bg-base-100">
+  <div class="navbar-start">
+    <div class="dropdown">
+      <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </div>
+      <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+      <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <li><a>O nás</a></li>
+        <li>
+          
+          <ul class="p-2">
+             <!-- svelte-ignore a11y-missing-attribute -->
+             <li><a>Interiér</a></li>
+             <!-- svelte-ignore a11y-missing-attribute -->
+             <li><a>Exteriér</a></li>
+              <!-- svelte-ignore a11y-missing-attribute -->
+             <li><a>Doplňky</a></li>
+          </ul>
+        </li>
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <li><a>Poptávka</a></li>
+      </ul>
+    </div>
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <a class="btn btn-ghost text-xl">Truhlářství DODO</a>
+  </div>
+  <div class="navbar-center hidden lg:flex">
+    <ul class="menu menu-horizontal px-1">
+      <!-- svelte-ignore a11y-missing-attribute -->
+      <li><a>O nás</a></li>
+      <li>
+        <details>
+          <summary>Co děláme?</summary>
+          <ul class="p-2">
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <li><a>Interiér</a></li>
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <li><a>Exteriér</a></li>
+             <!-- svelte-ignore a11y-missing-attribute -->
+            <li><a>Doplňky</a></li>
+          </ul>
+        </details>
+      </li>
+      <!-- svelte-ignore a11y-missing-attribute -->
+      <li><a>Poptávka</a></li>
+    </ul>
+  </div>
+  <div class="navbar-end">
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <a class="btn">Napsat nám email</a>
+  </div>
+</div>
