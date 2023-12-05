8a3ec49d2d8eb13156fa30635fefc39198f1690d
Daniel kodernamiru <kodernamiru@gmail.com>
2023-12-05 23:44:49 +0100

    Ver 2.0

--- src/lib/komponenty/Web/Hero.svelte
+++ src/lib/komponenty/Web/Hero.svelte
@@ -33,51 +33,50 @@
   });
 </script>
   
-  <style>
-    /* Základní styly pro obrázek */
-    .bg-image {
-      background-size: cover;
-      background-position: center;
-      background-repeat: no-repeat;
-      transition: opacity 0.5s ease-in-out, background 0.5s ease-in-out;
-      opacity: 1;
-      position: relative;
-      height: 100vh;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      text-align: center;
-    }
-  </style>
-  
-  <section class="relative">
-    <div class="bg-image" id="bgImage">
-      <div class="opacity-75 absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
-  
-      <div class="relative mx-auto max-w-screen-xl px-4 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
-        <div class="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
-          <div class="text-center-desktop">
-            <h1 class="text-3xl font-extrabold sm:text-5xl text-center text-white">
-              Truhlářství DODO
-              <strong class="block font-extraboldtext-center"></strong>
-            </h1>
-  
-            <h1 class="mt-4 max-w-lg sm:text-xl/relaxed text-white">
-              Kvalita v každém kousku: Truhlářství DODO - Vaše cesta k dokonalému designu
-            </h1>
-  
-            <div class="mt-8 flex flex-wrap gap-4 justify-center"> <!-- Upraveno zarovnání odkazů na střed -->
-              <a href="/" class="block rounded bg-black px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring">
-                Zavolat 
-              </a>
-  
-              <a href="/" class="block rounded bg-white px-12 py-3 text-sm font-medium text-black shadow hover:text-blackfocus:outline-none focus:ring">
-                Napsat Email
-              </a>
-            </div>
+<style>
+  /* Základní styly pro obrázek */
+  .bg-image {
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    transition: opacity 0.5s ease-in-out, background 0.5s ease-in-out;
+    opacity: 1;
+    position: relative;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+</style>
+
+<section class="relative">
+  <div class="bg-image" id="bgImage">
+    <div class="opacity-75 absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
+
+    <div class="relative mx-auto max-w-screen-xl px-4 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
+      <div class="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+        <div class="text-center-desktop">
+          <h1 class="text-3xl font-extrabold sm:text-5xl text-center text-white">
+            Truhlářství DODO
+            <strong class="block font-extraboldtext-center"></strong>
+          </h1>
+
+          <h1 class="mt-4 max-w-lg sm:text-xl/relaxed text-white">
+            Kvalita v každém kousku: Truhlářství DODO - Vaše cesta k dokonalému designu
+          </h1>
+
+          <div class="mt-8 flex flex-wrap gap-4 justify-center"> <!-- Upraveno zarovnání odkazů na střed -->
+            <a href="/" class="block rounded bg-black px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring">
+              Zavolat 
+            </a>
+
+            <a href="/" class="block rounded bg-white px-12 py-3 text-sm font-medium text-black shadow hover:text-blackfocus:outline-none focus:ring">
+              Napsat Email
+            </a>
           </div>
         </div>
       </div>
     </div>
-  </section>
-  
+  </div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,5 @@
 
 </script>
 
-<Hero/>
 <Info/>
 <ONas/>
