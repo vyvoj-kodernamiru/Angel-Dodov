d3686a15127e7346745787842c569a45395b7cb1
Daniel <kodernamiru@gmail.com>
2023-12-05 11:21:27 +0100

    HERO

--- src/lib/komponenty/Web/Hero.svelte
+++ src/lib/komponenty/Web/Hero.svelte
@@ -1,65 +1,79 @@
-<script lang="ts">
-    import { onMount, onDestroy } from 'svelte';
-    import { writable } from 'svelte/store';
+<script>
+    import { onMount } from 'svelte';
   
     const images = [
-      'https://firebasestorage.googleapis.com/v0/b/angel-dodov.appspot.com/o/HERO3.jpg?alt=media&token=9aa2b80f-6b14-4d9d-be52-1dcfba5a7aaf',
-      'https://firebasestorage.googleapis.com/v0/b/angel-dodov.appspot.com/o/HERO2.jpg?alt=media&token=09ac5980-192d-4e11-9c02-86f037ba0bd5',
+      'url(https://firebasestorage.googleapis.com/v0/b/angel-dodov.appspot.com/o/IMG_7369-min.jpeg?alt=media&token=7104a94f-b360-4999-b9b5-49e290a84fa0)',
+      
+      'url(https://firebasestorage.googleapis.com/v0/b/angel-dodov.appspot.com/o/IMG_7369-min.jpeg?alt=media&token=7104a94f-b360-4999-b9b5-49e290a84fa0',
       // Další URL obrázků...
     ];
   
-    const texts = [
-      {
-        title: 'Truhlářství DODO',
-        description: 'Kvalita v každém kousku: Truhlářství DODO - Vaše cesta k dokonalému designu',
-      },
-      // Další nadpisy a popisy...
-    ];
-  
-    let currentImageIndex = writable(0);
-  
-    let interval: NodeJS.Timeout;
+    let currentImageIndex = 0;
   
     onMount(() => {
-      interval = setInterval(() => {
-        currentImageIndex.update((val) => (val + 1) % images.length);
-      }, 5000); // Interval pro změnu obrázků (například každé 3 sekundy)
+      const interval = setInterval(() => {
+        const bgImageElement = document.getElementById('bgImage');
+        if (bgImageElement) {
+          bgImageElement.classList.add('bg-image-hidden');
+          setTimeout(() => {
+            currentImageIndex = (currentImageIndex + 1) % images.length;
+            if (bgImageElement) {
+              bgImageElement.style.backgroundImage = images[currentImageIndex];
+              bgImageElement.classList.remove('bg-image-hidden');
+            }
+          }, 500);
+        }
+      }, 5000);
   
-      return () => clearInterval(interval);
+      return () => clearInterval(interval); // Zastavit interval při odstranění komponenty
     });
-  
-    onDestroy(() => clearInterval(interval));
   </script>
   
   <style>
-    /* Styly pro plynulý přechod */
-    .image-transition {
-      opacity: 0;
-      transition: opacity 1s ease-in-out;
-      z-index: -1;
-    }
-  
-    .image-transition.active {
+    /* Základní styly pro obrázek */
+    .bg-image {
+      background-size: cover;
+      background-position: center;
+      background-repeat: no-repeat;
+      transition: opacity 0.5s ease-in-out, background 0.5s ease-in-out;
       opacity: 1;
-      z-index: 1;
+      position: relative;
+      height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      text-align: center;
     }
   </style>
   
-  <section class="relative pb-8" style="height: 750px;">
-    <div>
-      {#each images as image, index}
-        <img
-          src={image}
-          class="absolute top-0 bottom-0 left-0 right-0 object-cover w-full h-full image-transition {index === $currentImageIndex ? 'active' : ''}"
-          alt=""
-        />
-      {/each}
-      {#if texts.length > 0}
-        <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white z-10">
-          <h2 class="text-3xl font-bold md:text-4xl lg:text-7xl">{texts[$currentImageIndex].title}</h2>
-          <p class="text-base leading-8 lg:text-xl">{texts[$currentImageIndex].description}</p>
+  <section class="relative">
+    <div class="bg-image" id="bgImage">
+      <div class="opacity-75 absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
+  
+      <div class="relative mx-auto max-w-screen-xl px-4 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
+        <div class="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+          <div class="text-center-desktop">
+            <h1 class="text-3xl font-extrabold sm:text-5xl text-center text-white">
+              Truhlářství DODO
+              <strong class="block font-extrabold text-rose-700 text-center"></strong>
+            </h1>
+  
+            <p class="mt-4 max-w-lg sm:text-xl/relaxed text-white">
+              Kvalita v každém kousku: Truhlářství DODO - Vaše cesta k dokonalému designu
+            </p>
+  
+            <div class="mt-8 flex flex-wrap gap-4 justify-center"> <!-- Upraveno zarovnání odkazů na střed -->
+              <a href="/" class="block rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring">
+                Zavolat 
+              </a>
+  
+              <a href="/" class="block rounded bg-white px-12 py-3 text-sm font-medium text-rose-600 shadow hover:text-rose-700 focus:outline-none focus:ring">
+                Napsat Email
+              </a>
+            </div>
+          </div>
         </div>
-      {/if}
+      </div>
     </div>
   </section>
   
