c29888c80accc43efe09583efcefb6d72e606ec6
Daniel Kodernamiru <kodernamiru@gmail.com>
2023-12-21 09:12:27 +0100

    footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,13 @@
 	import Info from "$lib/komponenty/Web/Info.svelte";
 	import Navigation from "$lib/komponenty/Web/Navigation.svelte";
 	import Hero from "$lib/komponenty/Web/Hero.svelte";
+	import Footer from "$lib/komponenty/Web/Footer.svelte";
 </script>
 <Navigation/>
 <Hero/>
 
 <Info/>
 <ONas/>
+<footer>
+	<Footer/>
+</footer>
